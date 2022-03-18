0be8abd131ebec9c65d9085e27281f3d548be75a
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2022-03-18 14:37:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
+  require('phpQuery/phpQuery.php');
 
   $c = curl_init("https://uk.indeed.com/cmp/Game-Nation/jobs");
-	$raw = true;
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
-	$result = curl_exec($c);
-	curl_close($c);
+  $raw = true;
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+  $result = curl_exec($c);
+  curl_close($c);
 
-  $dom = new DOMDocument('1.0','UTF-8');
-  $dom->loadHTML($result);
-
-  $xpath = new DOMXpath($dom);
+  $doc = phpQuery::newDocumentHTML($result);
 
   $out = "";
   $out .= "<pre>";
@@ -23,20 +21,20 @@
     <description/>
     <language>en</language>
     </channel>";
-  $items = $xpath->query("//*[@data-testid='jobListItem']");
-  if(!is_null($items)){
-    foreach($items as $item){
-      //$title = $item->query("//*[@data-testid='jobListItem-title']/h3/button");
-      //$title = $xpath->query("//*[@data-testid='jobListItem'][$i]/div/div/div[@data-testid='jobListItem-title']/h3/button/text()");
+  //$items = $xpath->query("//*[@data-testid='jobListItem']");
+  foreach(pq('li[data-testid="jobListItem"]') as $item){
+    //$title = $item->query("//*[@data-testid='jobListItem-title']/h3/button");
+    //$title = $xpath->query("//*[@data-testid='jobListItem'][$i]/div/div/div[@data-testid='jobListItem-title']/h3/button/text()");
+
+    $title = pq($item)['div div div[data-testid="jobListItem-title"] h3 button'];
       
-      $out .= "<item>
-        <title></title>
-        <link>https://uk.indeed.com/cmp/Game-Nation/jobs</link>
-        <source>Game Nation</source>
-        <pubDate></pubDate>
-        <description></description>
-      </item>";
-    }
+    $out .= "<item>
+      <title>$title</title>
+      <link>https://uk.indeed.com/cmp/Game-Nation/jobs</link>
+      <source>Game Nation</source>
+      <pubDate></pubDate>
+      <description></description>
+    </item>";
   }
   
   $out .= '</xml>';
