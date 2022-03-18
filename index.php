bf7906b5046e720ce72dff1f03b731a281fa8aa7
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2022-03-18 14:39:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     //$title = $item->query("//*[@data-testid='jobListItem-title']/h3/button");
     //$title = $xpath->query("//*[@data-testid='jobListItem'][$i]/div/div/div[@data-testid='jobListItem-title']/h3/button/text()");
 
-    $title = pq($item)['div div div[data-testid="jobListItem-title"] h3 button'];
+    //$title = pq($item)['div div div[data-testid="jobListItem-title"] h3 button'];
       
     $out .= "<item>
       <title>$title</title>
