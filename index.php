b26802aa2e164935926d61e57ec45146091886db
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2022-03-18 14:40:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -21,21 +21,6 @@
     <description/>
     <language>en</language>
     </channel>";
-  //$items = $xpath->query("//*[@data-testid='jobListItem']");
-  foreach(pq('li[data-testid="jobListItem"]') as $item){
-    //$title = $item->query("//*[@data-testid='jobListItem-title']/h3/button");
-    //$title = $xpath->query("//*[@data-testid='jobListItem'][$i]/div/div/div[@data-testid='jobListItem-title']/h3/button/text()");
-
-    //$title = pq($item)['div div div[data-testid="jobListItem-title"] h3 button'];
-      
-    $out .= "<item>
-      <title>$title</title>
-      <link>https://uk.indeed.com/cmp/Game-Nation/jobs</link>
-      <source>Game Nation</source>
-      <pubDate></pubDate>
-      <description></description>
-    </item>";
-  }
   
   $out .= '</xml>';
   $out .= "</pre>";
